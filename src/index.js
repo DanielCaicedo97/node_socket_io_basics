de5215b11593f7ee68bb1c5200ffbeb0467947ad
Daniel Alejandro Caicedo Beanvides <caicedo.daniel@correounivalle.edu.co>
2023-11-17 12:54:23 -0500

    feat: manejo offline de eventos

--- src/index.js
+++ src/index.js
@@ -19,26 +19,14 @@ app.get("/", (req,res)=>{
     res.sendFile(__dirname + "/views/index.html");
 });
 
-// io.on("connection", socket => {
+io.on("connection", socket => {
+    socket.on("is_connected", msg =>{
+        console.log(msg);
+    })
 
-// });
-
-// Name personalizados
-const teachers = io.of("teachers");
-const students = io.of("students");
-
-teachers.on("connection", socket=>{
-    console.log(socket.id + " se ha conectado a la sala de profes");
-    socket.on("send_message", data =>{
-        teachers.emit("message", data);
-    });
 });
-students.on("connection", socket=>{
-    console.log(socket.id + " se ha conectado a la sala de estudiantes");
-    socket.on("send_message", data =>{
-        students.emit("message", data);
-    });
-});
+
+
 
 httpServer.listen(PORT, ()=>{
     console.log(`Server listen in port: http://localhost:${PORT} ...`)
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,42 +1,19 @@
-const user = prompt("Escribe tu usuario");
-const profes = ["Daniel","Alejandro","Caicedo"] // lista de usuarios de profesores
+const socket = io();
 
-let socketNameSpace, group;
+const send = document.getElementById("send");
+const disconnect = document.getElementById("disconnect");
+const reconnect = document.getElementById("reconnect");
 
-const chat = document.getElementById("chat");
-const namespace = document.getElementById("namespace");
 
-if(profes.includes(user)){
-    socketNameSpace = io("/teachers");
-    group = "teachers";
-    
-} else {
-    socketNameSpace = io("/students");
-    group = "students";
-}
-
-socketNameSpace.on("connect", ()=>{
-    namespace.textContent = group;
+send.addEventListener("click", ()=>{
+    if(socket.connected){
+        socket.emit("is_connected", "Esta conectado!");
+    }
 });
 
-// Progrmando la logica de envio de mensajes 
-
-const sendMessage = document.getElementById("sendMessage");
-
-sendMessage.addEventListener("click", ()=>{
-    const message = prompt("Escribe tu mensaje");
-    socketNameSpace.emit("send_message", {
-        message,
-        user
-    });
+disconnect.addEventListener("click", ()=>{
+    socket.disconnect();
 });
-
-
-socketNameSpace.on("message", data => {
-    const {message} = data; 
-    const {user} = data;
-
-    const li = document.createElement("li");
-    li.textContent = `${user}: ${message}`;
-    chat.append(li)
+reconnect.addEventListener("click", ()=>{
+    socket.connect();
 });
