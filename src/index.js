0999f6587c8160247858f9ab960f4602ca9a5d6b
Daniel Alejandro Caicedo Beanvides <caicedo.daniel@correounivalle.edu.co>
2023-11-17 07:50:18 -0500

    Primer commit: levantando el servidor y sus dependencias basicas

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import path from "path";
+import express from "express";
+import http from "http";
+import {fileURLToPath} from "url"
+import {Server as serverSocket} from "socket.io"
+
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+const httpServer = http.createServer(app);
+const io = new serverSocket(httpServer);
+
+app.use(express.static(path.join(__dirname,"views")));
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+io.on("connection", socket=>{
+    console.log(socket.id);
+});
+
+httpServer.listen(PORT, ()=>{
+    console.log(`Server listen in port: http://localhost:${PORT} ...`)
+});
