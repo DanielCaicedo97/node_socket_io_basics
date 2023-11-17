ba5b75af98aec1f95135c00df4a3dbba5667ec56
Daniel Alejandro Caicedo Beanvides <caicedo.daniel@correounivalle.edu.co>
2023-11-17 09:18:54 -0500

    feat: eventos  on once off

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const httpServer = http.createServer(app);
 const io = new serverSocket(httpServer);
+const socketsOnline = [];
+
 
 app.use(express.static(path.join(__dirname,"views")));
 
@@ -19,6 +21,9 @@ app.get("/", (req,res)=>{
 });
 
 io.on("connection", socket=>{
+
+    socketsOnline.push(socket.id);
+
     //Emision básica
     socket.emit("cualquier_nombre", "ahora estas conectado");
 
@@ -29,6 +34,30 @@ io.on("connection", socket=>{
 
     //Emision a  todos
     io.emit("a_todos", socket.id + "Se ha conectado");
+
+    //Emision a uno solo 
+    socket.on("last", data => {
+        const lastSocket = socketsOnline.pop();
+        io.to(lastSocket).emit("salute", data);
+    });
+
+    //Utilizando on, once, off
+    // socket.emit("on", "Emitiendo varias veces");
+    // socket.emit("on", "Emitiendo varias veces");
+
+    // socket.emit("once", "emitiendo una vez");
+    // socket.emit("once", "emitiendo una vez");
+
+
+    // off para apagar un evento
+    socket.emit("off", "emitiendo off");
+    
+    setTimeout(()=>{
+        socket.emit("off", "emitiendo off");
+    },3000)
+    
+
+
 });
 
 httpServer.listen(PORT, ()=>{
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -18,3 +18,35 @@ const emitToServer = document.getElementById("emit-to-server");
 emitToServer.addEventListener("click",()=>{
     socket.emit("server", "hola servidor")
 });
+
+//Emitir un evento al servidor
+const emitToLast = document.getElementById("emit-to-last");
+
+emitToLast.addEventListener("click",()=>{
+    socket.emit("last", "hola al último")
+});
+
+
+socket.on("salute", data =>{
+    console.log(data);
+});
+
+
+// on, once, off
+
+socket.on("on", ()=>{
+    console.log("se emite varias veces");
+});
+socket.once("once", ()=>{
+    console.log("se emite una sola vez");
+});
+
+const listener = () => {
+    console.log("El evento se apaga");
+};
+
+socket.on("off", listener);
+
+setTimeout(()=> {
+    socket.off("off", listener)
+}, 2000);
