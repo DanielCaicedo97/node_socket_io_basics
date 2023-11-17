f85d6d32b8c884937bc36146b20488913d2412af
Daniel Alejandro Caicedo Beanvides <caicedo.daniel@correounivalle.edu.co>
2023-11-17 10:06:25 -0500

    feat: broadcast de eventos + ejemplo moviendo circulo

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const httpServer = http.createServer(app);
 const io = new serverSocket(httpServer);
-const socketsOnline = [];
 
 
 app.use(express.static(path.join(__dirname,"views")));
@@ -22,42 +21,11 @@ app.get("/", (req,res)=>{
 
 io.on("connection", socket=>{
 
-    socketsOnline.push(socket.id);
+    socket.on("circle_position", position => {
+        socket.broadcast.emit("move_circle", position); // a todos menos a mi
+        // io.emit("move_circle", position);
 
-    //Emision básica
-    socket.emit("cualquier_nombre", "ahora estas conectado");
-
-    //Escuchando un evento del cliente
-    socket.on("server", data =>(
-        console.log(data)
-    ));
-
-    //Emision a  todos
-    io.emit("a_todos", socket.id + "Se ha conectado");
-
-    //Emision a uno solo 
-    socket.on("last", data => {
-        const lastSocket = socketsOnline.pop();
-        io.to(lastSocket).emit("salute", data);
     });
-
-    //Utilizando on, once, off
-    // socket.emit("on", "Emitiendo varias veces");
-    // socket.emit("on", "Emitiendo varias veces");
-
-    // socket.emit("once", "emitiendo una vez");
-    // socket.emit("once", "emitiendo una vez");
-
-
-    // off para apagar un evento
-    socket.emit("off", "emitiendo off");
-    
-    setTimeout(()=>{
-        socket.emit("off", "emitiendo off");
-    },3000)
-    
-
-
 });
 
 httpServer.listen(PORT, ()=>{
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,52 +1,34 @@
 const socket = io();
 
-// recibiendo el evento llamado en el server "cualquier_nombre"
-socket.on("cualquier_nombre", data =>{
-    console.log(data);
-    const text = document.getElementById("text"); // esto se puede eliminar
-    text.textContent = data;
+const circle = document.getElementById("circle");
+
+const drawCircle = position => {
+    circle.style.top = position.top;
+    circle.style.left= position.left;
+}
+
+const drag = e => {
+
+    const position = {
+        top: e.clientY + "px",
+        left: e.clientX + "px",
+    }
+    drawCircle(position);
+    socket.emit("circle_position", position);
+
+    // circle.style.top = clientY + "px"
+    // circle.style.left= clientX + "px"
+}
+
+document.addEventListener("mousedown", e => {
+    document.addEventListener("mousemove", drag);
+});
+
+document.addEventListener("mouseup", e => {
+    document.removeEventListener("mousemove", drag);
+});
+
+
+socket.on("move_circle", position => {
+    drawCircle(position);
 })
-
-socket.on("a_todos",data =>{
-    console.log(data)
-});
-
-
-//Emitir un evento al servidor
-const emitToServer = document.getElementById("emit-to-server");
-
-emitToServer.addEventListener("click",()=>{
-    socket.emit("server", "hola servidor")
-});
-
-//Emitir un evento al servidor
-const emitToLast = document.getElementById("emit-to-last");
-
-emitToLast.addEventListener("click",()=>{
-    socket.emit("last", "hola al último")
-});
-
-
-socket.on("salute", data =>{
-    console.log(data);
-});
-
-
-// on, once, off
-
-socket.on("on", ()=>{
-    console.log("se emite varias veces");
-});
-socket.once("once", ()=>{
-    console.log("se emite una sola vez");
-});
-
-const listener = () => {
-    console.log("El evento se apaga");
-};
-
-socket.on("off", listener);
-
-setTimeout(()=> {
-    socket.off("off", listener)
-}, 2000);
